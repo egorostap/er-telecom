042c0fe0c9a8d47740cb633879beacfc8646e496
egor_ostap <shake90@mail.ru>
2022-02-02 00:50:29 +0500

    .

--- zd_1.py
+++ zd_1.py
@@ -3,7 +3,8 @@
 import my_func
 
 # переменные с решениями
-sql_requests_zd_1 = ['''
+sql_requests_zd_1 = [
+    '''
     select distinct s_1.cl_id,
     (select sum(revenue_summ)/count(cl_id) as arpu
     from s_1 join s_2 using(cl_id)
@@ -40,59 +41,58 @@ sql_requests_zd_1 = ['''
     and cl_activ = 1
     ''',
 
-                     '''
-                     WITH A AS
-                     (
-                     select cl_id, product_name, product_probability,
-                     ROW_NUMBER() over (PARTITION by cl_id order by product_probability desc) as rownum
-                     from s_3
-                     )
-                     SELECT cl_id, product_name, sum(product_probability)/count(product_probability) as average_top_3_prod
-                     FROM A
-                     WHERE rownum <= 3
-                     group by cl_id
-                     ''',
+    '''
+    WITH A AS
+    (
+    select cl_id, product_name, product_probability,
+    ROW_NUMBER() over (PARTITION by cl_id order by product_probability desc) as rownum
+    from s_3
+    )
+    SELECT cl_id, product_name, sum(product_probability)/count(product_probability) as average_top_3_prod
+    FROM A
+    WHERE rownum <= 3
+    group by cl_id
+    ''',
 
-                     '''
-                     WITH t_0 AS
-                     (
-                     SELECT campaign_theme, count(channel_id) as ch_0 from s_1 where channel_id = 0 group by campaign_theme
-                     ),
-                     t_1 AS
-                     (
-                     SELECT campaign_theme, count(channel_id) as ch_1 from s_1 where channel_id = 1 group by campaign_theme
-                     ),
-                     t_2 AS 
-                     (
-                     SELECT campaign_theme, count(channel_id) as ch_2 from s_1 where channel_id = 2 group by campaign_theme
-                     ),
-                     t_3 AS 
-                     (
-                     SELECT campaign_theme, count(channel_id) as ch_3 from s_1 where channel_id = 3 group by campaign_theme
-                     ),
-                     t_4 AS 
-                     (
-                     SELECT campaign_theme, count(channel_id) as ch_4 from s_1 where channel_id = 4 group by campaign_theme
-                     ),
-                     t_5 AS 
-                     (
-                     SELECT campaign_theme, count(channel_id) as ch_5 from s_1 where channel_id = 5 group by campaign_theme
-                     ),
-                     t_6 AS 
-                     (
-                     SELECT campaign_theme, count(channel_id) as ch_6 from s_1 where channel_id = 6 group by campaign_theme
-                     )
-                     SELECT t_0.campaign_theme, ch_0, ch_1, ch_2, ch_3, ch_4, ch_5, ch_6
-                     FROM t_0
-                     left join t_1 using (campaign_theme)
-                     left join t_2 using (campaign_theme)
-                     left join t_3 using (campaign_theme)
-                     left join t_4 using (campaign_theme)
-                     left join t_5 using (campaign_theme)
-                     left join t_6 using (campaign_theme)
-                     
-                     '''
-                     ]
+    '''
+    WITH t_0 AS
+    (
+    SELECT campaign_theme, count(channel_id) as ch_0 from s_1 where channel_id = 0 group by campaign_theme
+    ),
+    t_1 AS
+    (
+    SELECT campaign_theme, count(channel_id) as ch_1 from s_1 where channel_id = 1 group by campaign_theme
+    ),
+    t_2 AS 
+    (
+    SELECT campaign_theme, count(channel_id) as ch_2 from s_1 where channel_id = 2 group by campaign_theme
+    ),
+    t_3 AS 
+    (
+    SELECT campaign_theme, count(channel_id) as ch_3 from s_1 where channel_id = 3 group by campaign_theme
+    ),
+    t_4 AS 
+    (
+    SELECT campaign_theme, count(channel_id) as ch_4 from s_1 where channel_id = 4 group by campaign_theme
+    ),
+    t_5 AS 
+    (
+    SELECT campaign_theme, count(channel_id) as ch_5 from s_1 where channel_id = 5 group by campaign_theme
+    ),
+    t_6 AS 
+    (
+    SELECT campaign_theme, count(channel_id) as ch_6 from s_1 where channel_id = 6 group by campaign_theme
+    )
+    SELECT t_0.campaign_theme, ch_0, ch_1, ch_2, ch_3, ch_4, ch_5, ch_6
+    FROM t_0
+    left join t_1 using (campaign_theme)
+    left join t_2 using (campaign_theme)
+    left join t_3 using (campaign_theme)
+    left join t_4 using (campaign_theme)
+    left join t_5 using (campaign_theme)
+    left join t_6 using (campaign_theme)
+    '''
+]
 
 # Запуск функции
 if __name__ == '__main__':
--- zd_2.py
+++ zd_2.py
@@ -8,7 +8,7 @@ left join p_4 using(client_id)'''
 # Запуск функции
 if __name__ == '__main__':
     # первый вариант решения: склеиваю таблицы в единую по ключу client_id, отправляю в гуглшитс, затем в GDS, формулы завожу в GDS
-    # https://docs.google.com/spreadsheets/d/1YDUfbpkDdT6ABQrmBjH79LNHG5pevZPYfs4dwAL2hyo/edit#gid=0 таблица с документом
-    # https://datastudio.google.com/reporting/10e31414-42c2-4cd5-a5b2-b0d6db06b69a ссылка на отчет в GDS
+    # таблица с документом: https://docs.google.com/spreadsheets/d/1YDUfbpkDdT6ABQrmBjH79LNHG5pevZPYfs4dwAL2hyo/edit#gid=0
+    # ссылка на отчет в GDS: https://datastudio.google.com/reporting/10e31414-42c2-4cd5-a5b2-b0d6db06b69a
     df = my_func.input_base(sql_request=zd_2)
     my_func.google_sheet_write(df)
